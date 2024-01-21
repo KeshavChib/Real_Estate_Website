12e0098a00da1b6aa8669749238c29477487afd0
KeshavChib <ks404536@gmail.com>
2024-01-21 07:24:10 -0800

    Changed config file

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,10 +1,10 @@
-import {auth} from 'express-oauth2-jwt-bearer'
+import {auth} from 'express-oauth2-jwt-bearer';
 
 const jwtCheck = auth({
     audience: process.env.AUDIENCE,
     issuerBaseURL: process.env.BASE_URL,
     tokenSigningAlg: "RS256"
-})
+});
 
 
 export default jwtCheck;
